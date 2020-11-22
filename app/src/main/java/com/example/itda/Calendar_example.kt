dd33f0e32985ef50e6da5985bd52baa9ef25974e
duyg5 <duyg5@naver.com>
2020-11-23 02:59:44 +0900

    1233
    1233

--- app/src/main/java/com/example/itda/Calendar_example.kt
+++ app/src/main/java/com/example/itda/Calendar_example.kt
@@ -1,156 +1,152 @@
 package com.example.itda
 
+
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.CalendarView
-import android.widget.CalendarView.OnDateChangeListener
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import kotlinx.android.synthetic.main.fragment_calendar.*
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.lang.reflect.Array.set
+import java.text.DateFormat
+import java.util.*
 
+class Calendar_example: AppCompatActivity() {
 
-class Calendar_example : AppCompatActivity() {
+    var fname: String = ""
+    var str: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        var fname: String? = null
-        var str: String? = null
-        var calendarView: CalendarView? = null
-        var cha_Btn: Button? = null
-        var del_Btn: Button? = null
-        var save_Btn: Button? = null
-        var diaryTextView: TextView? = null
-        var textView2: TextView? = null
-        var textView3: TextView? = null
-        var contextEditText: EditText? = null
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        calendarView = findViewById(R.id.calendarView)
-        diaryTextView = findViewById(R.id.diaryTextView)
-        save_Btn = findViewById(R.id.save_Btn)
-        del_Btn = findViewById(R.id.del_Btn)
-        cha_Btn = findViewById(R.id.cha_Btn)
-        textView2 = findViewById(R.id.textView2)
-        textView3 = findViewById(R.id.textView3)
-        contextEditText = findViewById(R.id.contextEditText)
-        //로그인 및 회원가입 엑티비티에서 이름을 받아옴
-        val intent = intent
-        val name = intent.getStringExtra("userName")
-        val userID = intent.getStringExtra("userID")
-            calendarView.setOnDateChangeListener(OnDateChangeListener { view, year, month, dayOfMonth ->
-            diaryTextView.visibility=View.VISIBLE
-            save_Btn.setVisibility(View.VISIBLE)
-            contextEditText.setVisibility(View.VISIBLE)
-            textView2.setVisibility(View.INVISIBLE)
-            cha_Btn.setVisibility(View.INVISIBLE)
-            del_Btn.setVisibility(View.INVISIBLE)
-            diaryTextView.setText(String.format("%d / %d / %d", year, month + 1, dayOfMonth))
-            contextEditText.setText("")
-            checkDay(year, month, dayOfMonth, userID)
-        })
-        save_Btn.setOnClickListener(View.OnClickListener {
-            saveDiary(fname)
-            str = contextEditText.getText().toString()
-            textView2.setText(str)
-            save_Btn.setVisibility(View.INVISIBLE)
-            cha_Btn.setVisibility(View.VISIBLE)
-            del_Btn.setVisibility(View.VISIBLE)
-            contextEditText.setVisibility(View.INVISIBLE)
-            textView2.setVisibility(View.VISIBLE)
-        })
+        setContentView(R.layout.fragment_calendar)
+        val calendar = Calendar.getInstance()
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+// 달력 날짜가 선택되면
+            diaryTextView.visibility = View.VISIBLE // 해당 날짜가 뜨는 textView가 Visible
+            save_Btn.visibility = View.VISIBLE // 저장 버튼이 Visible
+            contextEditText.visibility = View.VISIBLE // EditText가 Visible
+            textView2.visibility = View.INVISIBLE // 저장된 일기 textView가 Invisible
+            cha_Btn.visibility = View.INVISIBLE // 수정 Button이 Invisible
+            del_Btn.visibility = View.INVISIBLE // 삭제 Button이 Invisible
+            calendar.set(year,month,dayOfMonth)
+            val dateFormatter = DateFormat.getDateInstance(DateFormat.MEDIUM)
+            val formattedDate = dateFormatter.format(calendar.time)
+            diaryTextView.text = formattedDate
+//            diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+// 날짜를 보여주는 텍스트에 해당 날짜를 넣는다.
+            contextEditText.setText("") // EditText에 공백값 넣기
+
+            checkedDay(year, month, dayOfMonth) // checkedDay 메소드 호출
+
+
+        }
+
+        save_Btn.setOnClickListener { // 저장 Button이 클릭되면
+            saveDiary(fname) // saveDiary 메소드 호출
+//            toast(fname + "데이터를 저장했습니다.") // 토스트 메세지
+            str = contextEditText.getText().toString() // str 변수에 edittext내용을 toString
+//형으로 저장
+            textView2.text = "${str}" // textView에 str 출력
+            save_Btn.visibility = View.INVISIBLE
+            cha_Btn.visibility = View.VISIBLE
+            del_Btn.visibility = View.VISIBLE
+            contextEditText.visibility = View.INVISIBLE
+            textView2.visibility = View.VISIBLE
+        }
     }
 
-    fun checkDay(cYear: Int, cMonth: Int, cDay: Int, userID: String?) {
-        var fname: String? = null
-        var str: String? = null
-        var calendarView: CalendarView? = null
-        var cha_Btn: Button? = null
-        var del_Btn: Button? = null
-        var save_Btn: Button? = null
-        var diaryTextView: TextView? = null
-        var textView2: TextView? = null
-        var textView3: TextView? = null
-        var contextEditText: EditText? = null
-        fname = "" + userID + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt" //저장할 파일 이름설정
-        var fis: FileInputStream? = null //FileStream fis 변수
+    fun checkedDay(cYear: Int, cMonth: Int, cDay: Int) {
+        fname = "" + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"
+// 저장할 파일 이름 설정. Ex) 2019-01-20.txt
+        var fis: FileInputStream? = null // FileStream fis 변수 설정
+
         try {
-            fis = openFileInput(fname)
-            val fileData = ByteArray(fis.available())
-            fis.read(fileData)
+            fis = openFileInput(fname) // fname 파일 오픈!!
+
+            val fileData = ByteArray(fis.available()) // fileData에 파이트 형식
+//으로 저장
+            fis.read(fileData) // fileData를 읽음
             fis.close()
-            str = String(fileData)
-            contextEditText!!.visibility = View.INVISIBLE
-            textView2!!.visibility = View.VISIBLE
-            textView2!!.text = str
-            save_Btn!!.visibility = View.INVISIBLE
-            cha_Btn!!.visibility = View.VISIBLE
-            del_Btn!!.visibility = View.VISIBLE
-            cha_Btn!!.setOnClickListener {
-                contextEditText!!.visibility = View.VISIBLE
-                textView2!!.visibility = View.INVISIBLE
-                contextEditText!!.setText(str)
-                save_Btn!!.visibility = View.VISIBLE
-                cha_Btn!!.visibility = View.INVISIBLE
-                del_Btn!!.visibility = View.INVISIBLE
-                textView2!!.text = contextEditText!!.text
+
+            str = String(fileData) // str 변수에 fileData를 저장
+
+            contextEditText.visibility = View.INVISIBLE
+            textView2.visibility = View.VISIBLE
+            textView2.text = "${str}" // textView에 str 출력
+
+            save_Btn.visibility = View.INVISIBLE
+            cha_Btn.visibility = View.VISIBLE
+            del_Btn.visibility = View.VISIBLE
+
+            cha_Btn.setOnClickListener { // 수정 버튼을 누를 시
+                contextEditText.visibility = View.VISIBLE
+                textView2.visibility = View.INVISIBLE
+                contextEditText.setText(str) // editText에 textView에 저장된
+// 내용을 출력
+                save_Btn.visibility = View.VISIBLE
+                cha_Btn.visibility = View.INVISIBLE
+                del_Btn.visibility = View.INVISIBLE
+                textView2.text = "${contextEditText.getText()}"
             }
-            del_Btn!!.setOnClickListener {
-                textView2!!.visibility = View.INVISIBLE
-                contextEditText!!.setText("")
-                contextEditText!!.visibility = View.VISIBLE
-                save_Btn!!.visibility = View.VISIBLE
-                cha_Btn!!.visibility = View.INVISIBLE
-                del_Btn!!.visibility = View.INVISIBLE
+
+            del_Btn.setOnClickListener {
+                textView2.visibility = View.INVISIBLE
+                contextEditText.setText("")
+                contextEditText.visibility = View.VISIBLE
+                save_Btn.visibility = View.VISIBLE
+                cha_Btn.visibility = View.INVISIBLE
+                del_Btn.visibility = View.INVISIBLE
                 removeDiary(fname)
+//                toast(fname + "데이터를 삭제했습니다.")
             }
-            if (textView2!!.text == null) {
-                textView2!!.visibility = View.INVISIBLE
-                diaryTextView!!.visibility = View.VISIBLE
-                save_Btn!!.visibility = View.VISIBLE
-                cha_Btn!!.visibility = View.INVISIBLE
-                del_Btn!!.visibility = View.INVISIBLE
-                contextEditText!!.visibility = View.VISIBLE
+
+            if (textView2.getText() == "") {
+                textView2.visibility = View.INVISIBLE
+                diaryTextView.visibility = View.VISIBLE
+                save_Btn.visibility = View.VISIBLE
+                cha_Btn.visibility = View.INVISIBLE
+                del_Btn.visibility = View.INVISIBLE
+                contextEditText.visibility = View.VISIBLE
             }
+
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
     @SuppressLint("WrongConstant")
-    fun removeDiary(readDay: String?) {
+    fun saveDiary(readyDay: String) {
         var fos: FileOutputStream? = null
+
         try {
-            fos = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
-            val content = ""
+            fos = openFileOutput(readyDay, MODE_NO_LOCALIZED_COLLATORS)
+            var content: String = contextEditText.getText().toString()
             fos.write(content.toByteArray())
             fos.close()
+
         } catch (e: Exception) {
             e.printStackTrace()
         }
+
     }
 
     @SuppressLint("WrongConstant")
-    fun saveDiary(readDay: String?) {
-        var fname: String? = null
-        var str: String? = null
-        var calendarView: CalendarView? = null
-        var cha_Btn: Button? = null
-        var del_Btn: Button? = null
-        var save_Btn: Button? = null
-        var diaryTextView: TextView? = null
-        var textView2: TextView? = null
-        var textView3: TextView? = null
-        var contextEditText: EditText? = null
+    fun removeDiary(readyDay: String) {
         var fos: FileOutputStream? = null
+
         try {
-            fos = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
-            val content = contextEditText!!.text.toString()
+            fos = openFileOutput(readyDay, MODE_NO_LOCALIZED_COLLATORS)
+            var content: String = ""
             fos.write(content.toByteArray())
             fos.close()
+
         } catch (e: Exception) {
             e.printStackTrace()
         }
+
     }
 }
+
